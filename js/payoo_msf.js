d04ff7697e3a9e4a0af6478f30c7fc575825bf26
mahmud-reza-rafsun <rafsun16.it@gmail.com>
2024-12-03 12:24:08 +0600

    added event handler and prevent default

---
+++ js/payoo_msf.js
@@ -0,0 +1,8 @@
+// step-1: set event handler
+document.getElementById('login-btn').addEventListener('click', function(event){
+  //step-2: prevent default bhavior (prevent reloading browser).
+  event.preventDefault();
+  // step-3: get the phone number
+  const phoneNumber = document.getElementById('phone-number').value;
+  console.log(phoneNumber);
+});
